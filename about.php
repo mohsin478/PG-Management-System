0328eb7996de0b0125dce63fa90496e530943464
mohsin478 <shaikmohsin.edu@gmail.com>
2025-02-03 14:36:35 +0530

    Update about.php
    changed meta

--- about.php
+++ about.php
@@ -8,9 +8,7 @@
 <html lang="en">
 <head>
 <title> Intrend Interior Category Flat Bootstrap Responsive Website Template | About : W3layouts</title>
-	
-	<!-- Meta tag Keywords -->
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+
 	<meta charset="utf-8">
 	<meta name="keywords" content="Intrend Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
 	Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
